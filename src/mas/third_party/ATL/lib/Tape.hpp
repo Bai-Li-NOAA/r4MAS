40bd3b3eee3c39171f00002dec8eb74796b48734
msupernaw <matthew.supernaw@noaa.gov>
2021-05-07 13:41:02 -0400

    Update Tape.hpp

--- src/mas/third_party/ATL/lib/Tape.hpp
+++ src/mas/third_party/ATL/lib/Tape.hpp
@@ -72,7 +72,7 @@ namespace atl {
         uint32_t min_id = std::numeric_limits<uint32_t>::max();
         uint32_t max_id = std::numeric_limits<uint32_t>::min();
 
-        typedef tsl::hopscotch_set<VariableInfoPtr, atl::less_variable_info<REAL_T> > vi_storage;
+        typedef tsl::hopscotch_set<VariableInfoPtr/*, atl::less_variable_info<REAL_T>*/ > vi_storage;
         typedef typename vi_storage::iterator vi_iterator;
 //        typedef typename vi_storage::reverse_iterator vi_riterator;
         bool is_nl = false;
