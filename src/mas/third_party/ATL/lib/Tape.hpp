89d001006cacf11455db6da268cc9c5b0ad7042e
msupernaw <matthew.supernaw@noaa.gov>
2021-05-07 13:51:11 -0400

    Update Tape.hpp

--- src/mas/third_party/ATL/lib/Tape.hpp
+++ src/mas/third_party/ATL/lib/Tape.hpp
@@ -13,7 +13,6 @@
 
 #ifndef TAPE_HPP
 #define TAPE_HPP
-
 //#define ATL_THREAD_SAFE
 
 #include "Platform.hpp"
@@ -30,7 +29,6 @@
 #include <cmath>
 #include "Utilities/flat_set.hpp"
 #include "third_party/flat_hash_map/bytell_hash_map.hpp"
-#include "third_party/hopscotch-map/include/tsl/hopscotch_set.h"
 #include "Utilities/flat_map.hpp"
 
 namespace atl {
@@ -72,9 +70,9 @@ namespace atl {
         uint32_t min_id = std::numeric_limits<uint32_t>::max();
         uint32_t max_id = std::numeric_limits<uint32_t>::min();
 
-        typedef tsl::hopscotch_set<VariableInfoPtr/*, atl::less_variable_info<REAL_T>*/ > vi_storage;
+        typedef flat_set<VariableInfoPtr, atl::less_variable_info<REAL_T> > vi_storage;
         typedef typename vi_storage::iterator vi_iterator;
-//        typedef typename vi_storage::reverse_iterator vi_riterator;
+        typedef typename vi_storage::reverse_iterator vi_riterator;
         bool is_nl = false;
         vi_storage ids;
         vi_storage nl_ids;
@@ -146,7 +144,7 @@ namespace atl {
             first.resize(0);
             second.resize(0);
             third.resize(0);
-            ids.clear();//.clear_no_resize(); //clear();
+            ids.clear_no_resize(); //clear();
             pushed_ids.clear();
             id_list.clear();
             //            nl_ids.clear();
