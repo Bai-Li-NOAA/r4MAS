08e108fa9e75481b102ee5dce4106c4cce2b97a5
msupernaw <matthew.supernaw@noaa.gov>
2021-11-01 12:05:00 -0400

    Update Recruitment.hpp

--- src/mas/Recruitment.hpp
+++ src/mas/Recruitment.hpp
@@ -193,8 +193,7 @@ namespace mas {
         }
 
         virtual const REAL_T CalculateEquilibriumSpawningBiomass(REAL_T spawing_biomass_per_recruit) {
-            return (1.0 / this->GetBeta())*
-                    (std::log(spawing_biomass_per_recruit) * this->GetAlpha());
+            return (std::log(spawing_biomass_per_recruit * this->GetAlpha())/this->GetBeta());
         }
 
         virtual const REAL_T CalculateEquilibriumRecruitment(REAL_T equilibrium_spawning_biomass) {
@@ -346,8 +345,10 @@ namespace mas {
             return this->beta.GetValue();
         }
 
-        virtual const REAL_T CalculateEquilibriumSpawningBiomass(REAL_T spawing_biomass_per_recruit) {
-            return this->GetAlpha() * spawing_biomass_per_recruit - this->GetBeta();
+        virtual const variable CalculateEquilibriumSpawningBiomass(const variable& SB0 ,variable spawing_biomass_per_recruit) {
+         
+             return ( 4.0 * this->h* mas::exp(this->log_R0) * spawing_biomass_per_recruit-SB0 *(1.0-this->h)) / (5.0*this->h -1.0);
+            //   return this->GetAlpha() * spawing_biomass_per_recruit - this->GetBeta();
         }
 
         virtual const REAL_T CalculateEquilibriumRecruitment(REAL_T equilibrium_spawning_biomass) {
